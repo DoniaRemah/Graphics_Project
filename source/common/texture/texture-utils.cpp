47c475bbb7cb9cf1af3703072f787809abf937b9
Shahd1819 <shahd.arahman.l@gmail.com>
2022-12-15 21:42:59 +0200

    frag, texture-utils

--- source/common/texture/texture-utils.cpp
+++ source/common/texture/texture-utils.cpp
@@ -43,9 +43,9 @@ our::Texture2D *our::texture_utils::loadImage(const std::string &filename, bool 
     // TODO: (Req 5) Finish this function to fill the texture with the data found in "pixels"
 
     texture->bind();
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, size.x, size.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, pixels);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, size.x, size.y, 0, GL_RGBA, GL_UNSIGNED_BYTE, (void *)pixels);
 
-    if (generate_mipmap == 1)
+    if (generate_mipmap)
     {
         glGenerateMipmap(GL_TEXTURE_2D);
     }
