f2608084058092cba0dcba3b00f07070d6a3a2f2
Donia Remah <doniaremah@gmail.com>
2022-12-16 18:18:04 +0200

    Finished Forward Renderer.

--- source/common/systems/forward-renderer.cpp
+++ source/common/systems/forward-renderer.cpp
@@ -133,24 +133,57 @@ namespace our {
         // If there is no camera, we return (we cannot render without a camera)
         if(camera == nullptr) return;
 
+        auto owner = camera->getOwner();
+        auto local_world = owner->getLocalToWorldMatrix();
+        
+        glm::vec4 eye(0,0,0,1); // point (Position of camera)
+        glm::vec4 center(0,0,-1,1); //point (center of line of sight)
+
+        eye = local_world*eye; // Transforming to world space.
+        center = local_world*center; // Transforming to world space.
+
+        glm::vec3 cameraForward =glm::normalize(center-eye); // moving camera forward
+
         //TODO: (Req 9) Modify the following line such that "cameraForward" contains a vector pointing the camera forward direction
         // HINT: See how you wrote the CameraComponent::getViewMatrix, it should help you solve this one
-        glm::vec3 cameraForward = glm::vec3(0.0, 0.0, -1.0f);
         std::sort(transparentCommands.begin(), transparentCommands.end(), [cameraForward](const RenderCommand& first, const RenderCommand& second){
             //TODO: (Req 9) Finish this function
-            // HINT: the following return should return true "first" should be drawn before "second". 
-            return false;
+            // HINT: the following return should return true "first" should be drawn before "second".
+
+            // Dot product gets the perpenidcular distance between two vectors
+            auto First = glm::dot(cameraForward,first.center); // getting distance of the first entity
+            auto Second = glm::dot(cameraForward,second.center); // getting distance of the second.
+
+            // Assume x & y = 0 for simplicity
+            // If camera at z=3, one z=1, second z=2, z=2 is nearer. 
+            //After dot, z=-1 is greater, so it gets drawn first
+
+            if(First>Second ){ // The farthest gets Drawn first.
+                return true;
+            }else{
+                return false;
+            }  
         });
 
         //TODO: (Req 9) Get the camera ViewProjection matrix and store it in VP
+
+        glm::mat4 VP = camera->getProjectionMatrix(windowSize)*camera->getViewMatrix();
         
         //TODO: (Req 9) Set the OpenGL viewport using viewportStart and viewportSize
         
+        // First two parameters are lower left corner of the window.
+        glViewport(0,0,windowSize.x,windowSize.y);
+
         //TODO: (Req 9) Set the clear color to black and the clear depth to 1
-        
+
+        glClearColor(0.0f,0.0f,0.0f,1.0f);
+        glClearDepth(1.0);
         //TODO: (Req 9) Set the color mask to true and the depth mask to true (to ensure the glClear will affect the framebuffer)
         
-
+        // to override the buffers
+        glColorMask(true,true,true,true); 
+        glDepthMask(true);
+        
         // If there is a postprocess material, bind the framebuffer
         if(postprocessMaterial){
             //TODO: (Req 11) bind the framebuffer
@@ -159,9 +192,19 @@ namespace our {
 
         //TODO: (Req 9) Clear the color and depth buffers
         
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
         //TODO: (Req 9) Draw all the opaque commands
-        // Don't forget to set the "transform" uniform to be equal the model-view-projection matrix for each render command
+        // Don't forget to set the "transform" uniform 
+        // to be equal the model-view-projection matrix for each render command
         
+        for(auto command: opaqueCommands){
+            command.material->setup();
+            command.material->shader->set("transform",VP*command.localToWorld);
+            command.mesh->draw();
+        }
+
+
         // If there is a sky material, draw the sky
         if(this->skyMaterial){
             //TODO: (Req 10) setup the sky material
@@ -185,7 +228,11 @@ namespace our {
         }
         //TODO: (Req 9) Draw all the transparent commands
         // Don't forget to set the "transform" uniform to be equal the model-view-projection matrix for each render command
-        
+        for(auto command: transparentCommands){
+            command.material->setup();
+            command.material->shader->set("transform",VP*command.localToWorld);
+            command.mesh->draw();
+        }
 
         // If there is a postprocess material, apply postprocessing
         if(postprocessMaterial){
