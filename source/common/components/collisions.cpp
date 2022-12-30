429ecbf2d40f948d450521b054f94272638a643e
ranamoataz3 <ranamoataz02@gmail.com>
2022-12-30 02:29:22 +0200

    collisions class

---
+++ source/common/components/collisions.cpp
@@ -0,0 +1,23 @@
+#include "collisions.hpp"
+#include "../ecs/entity.hpp"
+#include "../deserialize-utils.hpp"
+
+namespace our
+{
+    // Reads linearVelocity & angularVelocity from the given json object
+    void CollisionComponent::deserialize(const nlohmann::json &data)
+    {
+        if (!data.is_object())
+            return;
+
+        std::string obstucaseTypeStr = data.value("obstucaseType", obstucaseType);
+        if (obstucaseTypeStr == "score")
+        {
+            obstucaseType = CollisionType::SCORE;
+        }
+        else
+        {
+            obstucaseType = CollisionType::WIN;
+        }
+    }
+}
