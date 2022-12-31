28c7374b688bbc789375ced4363024c191325822
Donia Remah <doniaremah@gmail.com>
2022-12-31 04:00:17 +0200

    added win & lose menu paths

--- source/states/win-state.hpp
+++ source/states/win-state.hpp
@@ -34,7 +34,7 @@ class Winstate: public our::State {
         menuMaterial->shader->attach("assets/shaders/textured.frag", GL_FRAGMENT_SHADER);
         menuMaterial->shader->link();
         // Then we load the menu texture
-        menuMaterial->texture = our::texture_utils::loadImage("assets/textures/ourMenu.png");
+        menuMaterial->texture = our::texture_utils::loadImage("assets/textures/winMenu.png");
         // Initially, the menu material will be black, then it will fade in
         menuMaterial->tint = glm::vec4(0.0f, 0.0f, 0.0f, 0.0f);
 
